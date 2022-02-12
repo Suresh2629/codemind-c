e0334c6387909572bbc4bbb4032979a7c703e873
Suresh2629 <97591413+Suresh2629@users.noreply.github.com>
2022-02-12 18:45:52 +0530

    #include <stdio.h>
    int main() {  
      int n,m;  
      scanf("%d",&n);  
      for(int i=n;i>=1;i--)  
      {  
        for(int j=1;j<=n;j++)  
        {  
           if(i==j || j==(n-i+1))  
           {  
             printf("%d ",i);  
           }  
           else  
           {  
             printf(" ");  
           }  
        }  
        printf("
    ");  
      }  
      return 0;  
    }

--- Print_the_following_pattern.c
+++ Print_the_following_pattern.c
@@ -1,15 +1,23 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=i;j<=n;j++)
-        printf("%d ",j);
-        printf("
-");
-    }
-    return 0;
-    
+#include <stdio.h>  
+  
+int main() {  
+  int n,m;  
+  scanf("%d",&n);  
+  for(int i=n;i>=1;i--)  
+  {  
+    for(int j=1;j<=n;j++)  
+    {  
+       if(i==j || j==(n-i+1))  
+       {  
+         printf("%d ",i);  
+       }  
+       else  
+       {  
+         printf(" ");  
+       }  
+    }  
+    printf("
+");  
+  }  
+  return 0;  
 }

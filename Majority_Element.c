a9eac2b6ca71179ed7032d80668ccb7c61560114
Suresh2629 <97591413+Suresh2629@users.noreply.github.com>
2022-04-30 21:30:17 +0530

    #include<stdio.h> int main() {     int x[20],i,j,n,count=0;     scanf("%d",&n);
        for(i=0;i<n;i++)
        {
            scanf("%d",&x[i]);
        }
        
        for(i=0;i<n;i++)
        { count=1;
        if(x[i]!=-1)
        {
            for(j=0;j<n;j++)
            {
                if(x[i]==x[j] && i!=j)
                {
                    count++;
                    x[j]=-1;
                }
            }
            if(count>n/2)
            {
                printf("%d",x[i]);
                return 0;
            }
        }
        }
        return 0;
    }

---
+++ Majority_Element.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int x[20],i,j,n,count=0;
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    
+    for(i=0;i<n;i++)
+    { count=1;
+    if(x[i]!=-1)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(x[i]==x[j] && i!=j)
+            {
+                count++;
+                x[j]=-1;
+            }
+        }
+        if(count>n/2)
+        {
+            printf("%d",x[i]);
+            return 0;
+        }
+    }
+    }
+    return 0;
+}

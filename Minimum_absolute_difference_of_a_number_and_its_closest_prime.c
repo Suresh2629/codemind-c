ba69ff4c1373fd2f74d51849b7d4d7096cb80946
Suresh2629 <97591413+Suresh2629@users.noreply.github.com>
2022-06-29 21:28:08 +0530

    #include<stdio.h> int main() {     int a,b,f=0,n,i,j,fwd,bwd;     scanf("%d",&n);     a=n;b=n;     while(1)     {         f=0;         for(i=1;i<=a;i++)         {             if(a%i==0)             {                 f++;             }         }         if(f==2)         {             fwd=a-n;             break;         }         a++;     }     while(1)     {         f=0;         for(i=1;i<=b;i++)         {             if(b%i==0)             {                 f++;             }         }         if(f==2)         {             bwd=n-b;             break;         }         b--;     }     if(fwd==0 && bwd==0)     {         printf("%d",fwd);     }else if(fwd<bwd)     {         printf("%d",fwd);     }else{         printf("%d",bwd);     }     return 0; }

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
@@ -1,53 +1,51 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-	int n,i,j,up,lp,s,m,l,upd,lpd;
-	scanf("%d",&n);
-	m=n;
-	for(m;;m++)
-	{
-		for(i=2;i<=sqrt(m);i++)
-		{
-			s=1;
-			if(m%i==0)
-			{
-				s=0;
-				break;
-			}
-		}
-		if(s==1)
-		{
-			up=m;
-			break;
-		}
-	}
-	l=n;
-	for(l;l>0;l--)
-	{
-		for(i=2;i<=sqrt(l);i++)
-		{
-			s=1;
-			if(l%i==0)
-			{
-				s=0;
-				break;
-			}
-		}
-		if(s==1)
-		{
-			lp=l;
-			break;
-		}
-	}
-	upd=up-n;
-	lpd=n-lp;
-	if(lpd<=upd)
-	{
-		printf("%d",lpd);
-	}
-	else
-	{
-		printf("%d",upd);
-	}
+    int a,b,f=0,n,i,j,fwd,bwd;
+    scanf("%d",&n);
+    a=n;b=n;
+    while(1)
+    {
+        f=0;
+        for(i=1;i<=a;i++)
+        {
+            if(a%i==0)
+            {
+                f++;
+            }
+        }
+        if(f==2)
+        {
+            fwd=a-n;
+            break;
+        }
+        a++;
+    }
+    while(1)
+    {
+        f=0;
+        for(i=1;i<=b;i++)
+        {
+            if(b%i==0)
+            {
+                f++;
+            }
+        }
+        if(f==2)
+        {
+            bwd=n-b;
+            break;
+        }
+        b--;
+    }
+    if(fwd==0 && bwd==0)
+    {
+        printf("%d",fwd);
+    }else if(fwd<bwd)
+    {
+        printf("%d",fwd);
+    }else{
+        printf("%d",bwd);
+    }
+    return 0;
 }

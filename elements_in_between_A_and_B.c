a8dfdc49e78ba69b9affc91e8154ff355169b53e
Suresh2629 <97591413+Suresh2629@users.noreply.github.com>
2022-06-29 22:49:37 +0530

    #include<stdio.h> int main() {     int n,a,b,ar[100],sum=0;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     scanf("%d%d",&a,&b);     for(int i=0;i<n;i++)     {         if(ar[i]>=a&&ar[i]<=b)         {           printf("%d ",ar[i]);           sum++;         }     }     if(sum==0)     printf("-1"); }

--- elements_in_between_A_and_B.c
+++ elements_in_between_A_and_B.c
@@ -1,25 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[50],s,e,sum=0,i,flag=0;
+    int n,a,b,ar[100],sum=0;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
+    for(int i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
-        
+        scanf("%d",&ar[i]);
     }
-    scanf("%d %d",&s,&e);
-    for(i=0;i<n;i++)
+    scanf("%d%d",&a,&b);
+    for(int i=0;i<n;i++)
     {
-        if(a[i]>=s && a[i]<=e)
+        if(ar[i]>=a&&ar[i]<=b)
         {
-            flag=1;
-            printf("%d ",a[i]);
+          printf("%d ",ar[i]);
+          sum++;
         }
     }
-    if(flag==0)
-    {
-        printf("-1");
-    }
-    
+    if(sum==0)
+    printf("-1");
 }

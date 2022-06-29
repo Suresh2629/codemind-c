d78abc0f06f4438d15eb2197625c89c37b78c274
Suresh2629 <97591413+Suresh2629@users.noreply.github.com>
2022-06-29 21:48:32 +0530

    #include<stdio.h> int main() {     int n,i,j,rev=0,r,temp,next,prev,diff;     scanf("%d",&n);     i=n+1;     j=n-1;     while(i>0)     {         rev=0;         temp=i;         while(temp>0)         {             r=temp%10;             rev=(rev*10)+r;             temp/=10;         }         if(i==rev)         {          next=i;          break;         }         i++;     }     while(j>0)     {         rev=0;         temp=j;         while(temp>0)         {             r=temp%10;             rev=(rev*10)+r;             temp/=10;         }         if(j==rev)         {          prev=j;          break;         }         j--;     }     if(next-n==n-prev)     {         printf("%d %d",prev,next);     }else if(next-n>n-prev)     {         printf("%d",prev);     }else{         printf("%d",next);     }     return 0; }

--- Closest_Palindrome_.c
+++ Closest_Palindrome_.c
@@ -1,54 +1,52 @@
 #include<stdio.h>
 int main()
 {
-	int n,q1,q2,k1,k2,rem1,rem2,res1,res2,pal1,pal2,l,u;
-	scanf("%d",&n);
-	k1=n-1;
-	for(k1;k1>0;k1--)
-	{
-		q1=k1;
-		res1=0;
-		while(q1>0)
-		{
-			rem1=q1%10;
-			res1=(res1*10)+rem1;
-			q1=q1/10;
-		}
-		if(res1==k1)
-		{
-			pal1=k1;
-			break;
-		}
-	}
-	k2=n+1;
-	for(k2;;k2++)
-	{
-		q2=k2;
-		res2=0;
-		while(q2>0)
-		{
-			rem2=q2%10;
-			res2=(res2*10)+rem2;
-			q2=q2/10;
-		}
-		if(res2==k2)
-		{
-			pal2=k2;
-			break;
-		}
-	}
-    l=n-pal1;
-    u=pal2-n;
-    if(l>u)
+    int n,i,j,rev=0,r,temp,next,prev,diff;
+    scanf("%d",&n);
+    i=n+1;
+    j=n-1;
+    while(i>0)
     {
-    	printf("%d",pal2);
-	}
-	else if(l==u)
-	{
-		printf("%d %d",pal1,pal2);
-	}
-	else
-	{
-		printf("%d",pal1);
-	}
+        rev=0;
+        temp=i;
+        while(temp>0)
+        {
+            r=temp%10;
+            rev=(rev*10)+r;
+            temp/=10;
+        }
+        if(i==rev)
+        {
+         next=i;
+         break;
+        }
+        i++;
+    }
+    while(j>0)
+    {
+        rev=0;
+        temp=j;
+        while(temp>0)
+        {
+            r=temp%10;
+            rev=(rev*10)+r;
+            temp/=10;
+        }
+        if(j==rev)
+        {
+         prev=j;
+         break;
+        }
+        j--;
+    }
+    if(next-n==n-prev)
+    {
+        printf("%d %d",prev,next);
+    }else if(next-n>n-prev)
+    {
+        printf("%d",prev);
+    }else{
+        printf("%d",next);
+    }
+    return 0;
 }

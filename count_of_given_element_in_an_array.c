c5c395813037144fc634949396619390c2097ce8
Suresh2629 <97591413+Suresh2629@users.noreply.github.com>
2022-06-30 19:08:23 +0530

    #include<stdio.h> int main() { 	int ar[20],n,f=0,i,se; 	scanf("%d",&n); 	for(i=0;i<n;i++) 	{ 		scanf("%d",&ar[i]); 	} 	scanf("%d",&se); 	for(i=0;i<n;i++) 	{ 		if(ar[i]==se) 		{ 			f++; 		}
    	}
    	printf("%d",f);
    }

--- count_of_given_element_in_an_array.c
+++ count_of_given_element_in_an_array.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,num,occr=0;
-    scanf("%d",&n);
-    int arr[n];
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    scanf("%d",&num);
-    for(i=0;i<n;i++)
-    {
-        if(arr[i]==num)
-        {
-            occr++;
-        }
-    }
-    printf("%d",occr);
+	int ar[20],n,f=0,i,se;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&ar[i]);
+	}
+	scanf("%d",&se);
+	for(i=0;i<n;i++)
+	{
+		if(ar[i]==se)
+		{
+			f++;
+		}
+		
+	}
+	printf("%d",f);
 }

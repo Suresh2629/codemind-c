6d4215bd53c7ab23fd0a13ae9eeaffbbcf64421a
Suresh2629 <97591413+Suresh2629@users.noreply.github.com>
2022-06-30 18:55:57 +0530

    #include<stdio.h> int main() {     int n,a[100],c=0;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }      for(int i=1;i<n-1;i++)     {        if(a[i-1]%2==0&&a[i+1]%2==0)        {            if(a[i]%2!=0)            {                c++;            }        }     }     printf("%d",c);
    }

--- count_odd_numbers_between_even_numbers.c
+++ count_odd_numbers_between_even_numbers.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[50],c=0,i;
+    int n,a[100],c=0;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
+    for(int i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i=0;i<n-2;i++)
+     for(int i=1;i<n-1;i++)
     {
-        if((a[i]%2==0 and a[i+2]%2==0))
-        {
-            if(a[i+1]%2!=0)
-            {
-                c++;
-            }
-        }
+       if(a[i-1]%2==0&&a[i+1]%2==0)
+       {
+           if(a[i]%2!=0)
+           {
+               c++;
+           }
+       }
     }
     printf("%d",c);
-    return 0;
+    
 }
